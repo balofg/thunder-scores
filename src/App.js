cb11c386870d72df477b42f11390adbf49dd4f33
balofg <balofg@live.com>
2018-04-22 17:54:05 +0200

    thunder customization

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="hero is-large">
+        <div className="hero-body">
+          <div className="container">
+            <div className="title">
+              Thunder Score Keeper
+            </div>
+            <div className="subtitle">
+              Automatic score keeper for the "Thunder" card game
+            </div>
+            <div className="body">
+              <button className="button is-primary">
+                <span className="icon is-small">
+                  <i className="fas fa-bolt" />
+                </span>
+                <span>New Game</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import '@fortawesome/fontawesome';
+import '@fortawesome/fontawesome-free-solid';
+import 'bulma/css/bulma.min.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
