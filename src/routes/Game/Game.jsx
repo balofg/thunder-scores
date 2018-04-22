598292f937ea0cb9b9f8e5016247927573983165
balofg <balofg@live.com>
2018-04-22 23:49:05 +0200

    hand dealing ui

--- src/routes/Game/Game.jsx
+++ src/routes/Game/Game.jsx
@@ -3,17 +3,62 @@ import PropTypes from 'prop-types';
 
 class Game extends Component {
   render() {
-    const { players } = this.props;
+    const { players, hand } = this.props;
     return (
       <div className="section">
         <div className="container">
-          <div className="content">
-            <div className="field is-grouped">
-
+          {hand && hand.status === 'OPEN' ? (
+            <div className="content">
+              <h1 className="is-size-3">
+                {hand.cardsCount} card{hand.cardsCount > 1 ? 's' : ''}
+              </h1>
             </div>
-          </div>
+          ) : (
+            <div className="content">
+              <div className="columns">
+                <div className="column">
+                  <div className="field">
+                    <div className="control">
+                      <label className="label">Number of cards</label>
+                      <input
+                        className="input"
+                        type="tel"
+                      />
+                    </div>
+                  </div>
+                </div>
 
-          <div className="content">
+                <div className="column">
+                  <div className="field">
+                    <div className="control">
+                      <label className="label">Dealer</label>
+                      <div className="select" style={{ width: '100%' }}>
+                        <select style={{ width: '100%' }}>
+                          {players.map(player => (
+                            <option value={player.id} key={player.id}>{player.name}</option>
+                          ))}
+                        </select>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="column" style={{ display: 'flex' }}>
+                  <button
+                    className="button is-primary"
+                    style={{ alignSelf: 'flex-end' }}
+                  >
+                    <span className="icon is-small">
+                      <i className="fas fa-hand-paper" />
+                    </span>
+                    <span>Deal hand</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
+          <div className="content has-text-centered">
             {players.map(player => (
               <div className="card" key={player.id}>
                 <div className="card-content">
@@ -33,12 +78,30 @@ class Game extends Component {
   }
 }
 
+const betShape = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  playerId: PropTypes.string.isRequired,
+  status: PropTypes.oneOf(['OPEN', 'WON', 'LOST']).isRequired,
+  value: PropTypes.number.isRequired,
+});
+
 Game.propTypes = {
   players: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
     score: PropTypes.number.isRequired,
+    bet: betShape,
   })).isRequired,
+  hand: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    dealerId: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+  }),
+  bets: PropTypes.arrayOf(betShape).isRequired,
+};
+
+Game.defaultProps = {
+  hand: undefined,
 };
 
 export default Game;
--- src/routes/Game/GameContainer.js
+++ src/routes/Game/GameContainer.js
@@ -1,11 +1,24 @@
 import { connect } from 'react-redux';
+
+import { dealHand, placeBet, winBet, loseBet, closeHand } from '../../store/actions';
 import GameComponent from './Game.jsx';
 
 const mapStateToProps = state => ({
   players: state.players.map(player => ({
     ...player,
     score: player.scores[player.scores.length - 1].score,
+    bet: state.bets.find(bet => bet.status === 'OPEN' && bet.playerId === player.id),
   })),
+  hand: state.hands.find(hand => hand.status === 'OPEN'),
+  bets: state.bets.find(bet => bet.status === 'OPEN'),
 });
 
-export default connect(mapStateToProps, null)(GameComponent);
+const mapDispatchToProps = {
+  dealHand,
+  placeBet,
+  winBet,
+  loseBet,
+  closeHand,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameComponent);
