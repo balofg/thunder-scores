cdc4c6b37fa967b366d42f318aad9c8a92116db1
balofg <balofg@live.com>
2018-04-24 01:19:44 +0200

    scores back to game logic

--- src/routes/Scoreboard/Scoreboard.jsx
+++ src/routes/Scoreboard/Scoreboard.jsx
@@ -1,54 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { history as historyPropTypes } from 'history-prop-types';
 
-const Scoreboard = props => (
-  <div className="section">
-    <div className="container">
-      <div className="content">
-        <h1 className="is-size-3">Scoreboard</h1>
-      </div>
+class Scoreboard extends Component {
+  componentWillMount() {
+    if (!this.props.players || this.props.players) {
+      this.props.history.push('players');
+    }
+  }
 
-      <table className="table is-striped is-fullwidth">
-        <thead>
-          <tr>
-            <th>Cards</th>
-            {props.players.map(player => (
-              <th key={player.id}>{player.name}</th>
-            ))}
-          </tr>
-        </thead>
+  render() {
+    const { players, hands, history } = this.props;
+    return (
+      <div className="section">
+        <div className="container">
+          <div className="content">
+            <h1 className="is-size-3">Scoreboard</h1>
+          </div>
 
-        <tbody>
-          {props.hands.map(hand => (
-            <tr key={`${hand.id}`}>
-              <td>
-                <strong>{hand.cards}</strong>
-              </td>
-              {props.players.map(({ id }) => {
-                const bet = hand.bets.find(({ player }) => player === id);
-                return (
-                  <td
-                    key={id}
-                    className={`has-text-white has-background-${bet.hasLost ? 'danger' : 'success'}`}
-                  >
-                    {bet.isDealer && (
-                      <span className="icon" style={{ marginRight: '15px' }}>
-                        <i className="fas fa-star" />
-                      </span>
-                    )}
+          <table className="table is-striped is-fullwidth">
+            <thead>
+              <tr>
+                <th>Cards</th>
+                {players.map(player => (
+                  <th key={player.id}>{player.name}</th>
+                ))}
+              </tr>
+            </thead>
 
-                    <span>{bet.score}</span>
+            <tbody>
+              {!hands.length && (
+                <tr>
+                  <td className="has-text-grey has-text-centered" colSpan={players.length + 1}>
+                    Nothing to display
                   </td>
-                );
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </div>
-);
+                </tr>
+              )}
+              {hands.map(hand => (
+                <tr key={`${hand.id}`}>
+                  <td>
+                    <strong>{hand.cards}</strong>
+                  </td>
+                  {players.map(({ id }) => {
+                    const bet = hand.bets.find(({ player }) => player === id);
+                    return (
+                      <td
+                        key={id}
+                        className={`has-text-white has-background-${bet.hasLost ? 'danger' : 'success'}`}
+                      >
+                        {bet.isDealer && (
+                          <span className="icon" style={{ marginRight: '15px' }}>
+                            <i className="fas fa-star" />
+                          </span>
+                        )}
+
+                        <span>{bet.score}</span>
+                      </td>
+                    );
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="content has-text-right">
+            <button
+              className="button"
+              onClick={() => history.push('game')}
+            >
+              <span className="icon is-small">
+                <i className="fas fa-arrow-left" />
+              </span>
+              <span>Back to game</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 Scoreboard.propTypes = {
   hands: PropTypes.arrayOf(PropTypes.shape({
