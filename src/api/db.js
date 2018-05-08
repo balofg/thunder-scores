8b5bd764fe34796d77a1cb25bef5f5006040b8bf
balofg <balofg@live.com>
2018-05-09 00:14:10 +0200

    change db singleton from class to es module

--- src/api/db.js
+++ src/api/db.js
@@ -1,126 +1,133 @@
 import idb from 'idb';
 
-class DB {
-  constructor() {
-    this.db = idb.open('thunder', 1, (upgradeDb) => {
-      const gamesStore = upgradeDb.createObjectStore('games', { keyPath: 'id' });
-      gamesStore.createIndex('status', 'status', { unique: false });
+// eslint-disable-next-line no-underscore-dangle
+const _db = idb.open('thunder', 1, (upgradeDb) => {
+  const gamesStore = upgradeDb.createObjectStore('games', { keyPath: 'id' });
+  gamesStore.createIndex('status', 'status', { unique: false });
 
-      const playersStore = upgradeDb.createObjectStore('players', { keyPath: 'id' });
-      playersStore.createIndex('game', 'gameId', { unique: false });
+  const playersStore = upgradeDb.createObjectStore('players', { keyPath: 'id' });
+  playersStore.createIndex('game', 'gameId', { unique: false });
 
-      const handsStore = upgradeDb.createObjectStore('hands', { keyPath: 'id' });
-      handsStore.createIndex('game', 'gameId', { unique: false });
-      handsStore.createIndex('dealer', 'dealerId', { unique: false });
+  const handsStore = upgradeDb.createObjectStore('hands', { keyPath: 'id' });
+  handsStore.createIndex('game', 'gameId', { unique: false });
+  handsStore.createIndex('dealer', 'dealerId', { unique: false });
 
-      const betsStore = upgradeDb.createObjectStore('bets', { keyPath: 'id' });
-      betsStore.createIndex('hand', 'handId', { unique: false });
-      betsStore.createIndex('player', 'playerId', { unique: false });
-    });
-  }
+  const betsStore = upgradeDb.createObjectStore('bets', { keyPath: 'id' });
+  betsStore.createIndex('hand', 'handId', { unique: false });
+  betsStore.createIndex('game', 'gameId', { unique: false });
+});
 
-  async getOpenGames() {
-    const db = await this.db;
-    const transaction = db.transaction(['games']);
+export const getOpenGames = async () => {
+  const db = await _db;
+  const transaction = db.transaction(['games']);
 
-    transaction
-      .objectStore('games')
-      .index('status')
-      .getAll('OPEN');
+  transaction
+    .objectStore('games')
+    .index('status')
+    .getAll('OPEN');
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async getPlayersByGame(gameId) {
-    const db = await this.db;
-    const transaction = db.transaction(['players']);
+export const getClosedGames = async () => {
+  const db = await _db;
+  const transaction = db.transaction(['games']);
 
-    transaction
-      .objectStore('players')
-      .index('game')
-      .getAll(gameId);
+  transaction
+    .objectStore('games')
+    .index('status')
+    .getAll('CLOSED');
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async getHandsByGame(gameId) {
-    const db = await this.db;
-    const transaction = db.transaction(['hands']);
+export const getPlayersByGame = async (gameId) => {
+  const db = await _db;
+  const transaction = db.transaction(['players']);
 
-    transaction
-      .objectStore('hands')
-      .index('game')
-      .getAll(gameId);
+  transaction
+    .objectStore('players')
+    .index('game')
+    .getAll(gameId);
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async getBetsByHand(handId) {
-    const db = await this.db;
-    const transaction = db.transaction(['bets']);
+export const getHandsByGame = async (gameId) => {
+  const db = await _db;
+  const transaction = db.transaction(['hands']);
 
-    transaction
-      .objectStre('bets')
-      .index('hand')
-      .getAll(handId);
+  transaction
+    .objectStore('hands')
+    .index('game')
+    .getAll(gameId);
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async getBetsByPlayer(playerId) {
-    const db = await this.db;
-    const transaction = db.transaction(['bets']);
+export const getBetsByHand = async (handId) => {
+  const db = await _db;
+  const transaction = db.transaction(['bets']);
 
-    transaction
-      .objectStore('bets')
-      .index('player')
-      .getAll(playerId);
+  transaction
+    .objectStre('bets')
+    .index('hand')
+    .getAll(handId);
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async saveGame(game) {
-    const db = await this.db;
-    const transaction = db.transaction(['games'], 'readwrite');
+export const getBetsByPlayer = async (playerId) => {
+  const db = await _db;
+  const transaction = db.transaction(['bets']);
 
-    transaction
-      .objectStore('games')
-      .put(game);
+  transaction
+    .objectStore('bets')
+    .index('player')
+    .getAll(playerId);
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async savePlayers(players) {
-    const db = await this.db;
-    const transaction = db.transaction(['players'], 'readwrite');
-    const store = transaction.objectStore('players');
+export const saveGame = async (game) => {
+  const db = await _db;
+  const transaction = db.transaction(['games'], 'readwrite');
 
-    players.forEach(player => store.put(player));
+  transaction
+    .objectStore('games')
+    .put(game);
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async saveHand(hand) {
-    const db = await this.db;
-    const transaction = db.transaction(['hands'], 'readwrite');
+export const savePlayers = async (players) => {
+  const db = await _db;
+  const transaction = db.transaction(['players'], 'readwrite');
+  const store = transaction.objectStore('players');
 
-    transaction
-      .objectStore('hands')
-      .put(hand);
+  players.forEach(player => store.put(player));
 
-    return transaction.complete;
-  }
+  return transaction.complete;
+};
 
-  async saveBet(bet) {
-    const db = await this.db;
-    const transaction = db.transaction(['bets'], 'readwrite');
+export const saveHand = async (hand) => {
+  const db = await _db;
+  const transaction = db.transaction(['hands'], 'readwrite');
 
-    transaction
-      .objectStore('bets')
-      .put(bet);
+  transaction
+    .objectStore('hands')
+    .put(hand);
 
-    return transaction.complete;
-  }
-}
+  return transaction.complete;
+};
 
-export default new DB();
+export const saveBet = async (bet) => {
+  const db = await _db;
+  const transaction = db.transaction(['bets'], 'readwrite');
+
+  transaction
+    .objectStore('bets')
+    .put(bet);
+
+  return transaction.complete;
+};
